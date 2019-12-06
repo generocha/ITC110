11d6406df994fdffb9fe78afc5403c78db66b4fe
generocha <gene@generocha.com>
2019-12-05 22:57:55 -0800

    update assignment 8 files and gitignore

--- assignment8/grade.py
+++ assignment8/grade.py
@@ -4,19 +4,21 @@ Grade Class
 2. get the credits
 3. get the grade points
 Gene Rocha
-12/4/2019
+12/5/2019
 '''
+# Class initiallization. 
+# Pass in and set the values
 class Grade():
     def __init__(self, class_name, class_credits, grade_point):
         self.class_name = class_name
         self.class_credits = class_credits
         self.grade_point = grade_point
-
+    # get the class name
     def getClassName(self):
         return self.class_name
-
+    # get the class credits
     def getCredits(self):
         return self.credits
-
+    # get the grade points given for the class
     def getGradePoint(self):
         return self.grade_point
--- assignment8/student_grades.py
+++ assignment8/student_grades.py
@@ -1,15 +1,21 @@
+# import the grade and student classes
 from grade import Grade
 from student import Student
-
+# add the student name and id
 def main():
     student = Student("Sally Record", "1234")
     cont ='y'
     while cont == 'y':
+        # get the name, credit and grade point given for the class
         className = input("Enter the class name: ")
         credit = int(input("Enter the credits for the class: "))
         gradePoint = float(input("Enter the grade points for the class: "))
+        # pass the name, credit and grade point given for the class to the Grade class
         grade = Grade(className,credit,gradePoint)
+        # pass grade info to the student setter
+        student.setGrade(grade)
         cont = input("Do you want to add another class grade?: ")
+        cont = cont.lower()
     print(student)
 
 main()
